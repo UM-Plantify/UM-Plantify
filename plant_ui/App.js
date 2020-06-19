e8731886d568896dddcc7a86d9ae546e94e81d35
lewis-camille <64331994+lewis-camille@users.noreply.github.com>
2020-06-19 12:41:37 -0400

    ...

--- plant_ui/App.js
+++ plant_ui/App.js
@@ -1,57 +1,28 @@
-// @flow
+// In App.js in a new project
+
 import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/components/home';
 
-// *** Just a static screen w/ text on it
-/* class App extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.titleText}>
-                    Plant Identification Application   
-                </Text>
-            </View>
-        );
-    }
-} */
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
 const Stack = createStackNavigator();
 
 function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-export default App;
-/* export default StackNavigator({
-    Home: { screen: HomeScreen,
-        navigationOptions: {
-            title: 'Home',
-            headerBackTitle: 'Back',
-        },
-    },
-    Detail: {screen: DetailScreen,
-        navigationOptions: {
-            title: 'Detail'
-        },
-    }
-}); */
 
-/* const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    titleText: {
-      fontWeight: 'bold',
-      fontSize: 50
-    }
-  }); */
+export default App;
